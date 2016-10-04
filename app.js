349e92bb361f889359ab3afa87e7fcaeb58f3caf
Si Taggart <me@simontaggart.com>
2016-10-04 13:16:07 -0700

    File headers, remove basic-auth  and some more boilerplate removal

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var auth = require('basic-auth');
+/*!
+ * Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 var compression = require('compression');
 var express = require('express');
 
@@ -6,27 +16,6 @@ var port = process.env.PORT || 9001;
 
 // Create server
 var app = express();
-
-if (process.env.NODE_ENV === 'production') {
-  // Authentication
-  app.use(function (req, res, next) {
-    var user = auth(req)
-    var username = process.env.AUTH_USERNAME;
-    var password = process.env.AUTH_PASSWORD;
-
-    var unauthorized = function () {
-      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-      res.sendStatus(401);
-    }
-
-    if (!username || !password) return unauthorized();
-    if (!user || user.name !== username || user.pass !== password) {
-      return unauthorized();
-    }
-
-    next();
-  });
-}
 
 // Compress all files
 app.use(compression());
