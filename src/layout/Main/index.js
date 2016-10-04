eb7e16bac9de7c4c4b2c39167730c7c52bad2dfa
Si Taggart <me@simontaggart.com>
2016-10-04 13:06:05 -0700

    Remove boilplate and unused routes and holding pages for demo

--- src/layout/Main/index.js
+++ src/layout/Main/index.js
@@ -1,30 +1,10 @@
 import React, {Component, PropTypes} from 'react';
-import { Link } from 'react-router';
 
 class MainLayout extends Component {
   render () {
     return (
       <div className="slds-container--large slds-container--center">
         {React.cloneElement(this.props.children, {})}
-        <nav role="navigation">
-          <ul className="slds-list--horizontal slds-has-dividers--left slds-has-block-links--space">
-            <li className="slds-item">
-              <Link to="/">{'Home'}</Link>
-            </li>
-            <li className="slds-item">
-              <Link to="/sorting-list">{'Sorting a List'}</Link>
-            </li>
-            <li className="slds-item">
-              <Link to="/resize">{'Resizing a Column'}</Link>
-            </li>
-            <li className="slds-item">
-              <Link to="/kanban">{'Kanban Style Card Moving'}</Link>
-            </li>
-            <li className="slds-item">
-              <Link to="/object-on-canvas">{'Changing an Object on a Canvas'}</Link>
-            </li>
-          </ul>
-        </nav>
       </div>
     )
   }
--- src/views/SortingList/index.js
+++ src/views/SortingList/index.js
@@ -6,50 +6,48 @@ class SortingListView extends Component {
   constructor (props) {
     super(props);
 
-    this.state = {
-      listItems: [{
-        id: 1,
-        title: 'Sales Cloud',
-        initials: 'SC',
-        description: 'The primary internal Salesforce org. Used to run our...'
-      }, {
-        id: 2,
-        title: 'Marketing Cloud',
-        initials: 'MC',
-        description: 'Salesforce Marketing Cloud lets businesses of any size...'
-      }, {
-        id: 3,
-        title: 'HR Concierge',
-        initials: 'HR',
-        description: 'Community for managing employee benefits and time off.'
-      }, {
-        id: 4,
-        title: 'My Money',
-        initials: 'MM',
-        description: 'Manage your finances across multiple financial platforms...'
-      }, {
-        id: 5,
-        title: 'Call Center',
-        initials: 'CC',
-        description: 'The key to call center and contact center management is more...'
-      }, {
-        id: 6,
-        title: 'Customer Support Community',
-        initials: 'CS',
-        description: 'Areas of Focus are used to track customer support for your...'
-      }, {
-        id: 7,
-        title: 'Salesforce Chatter',
-        initials: 'CH',
-        description: 'The Salesforce Chatter social network, including profiles and feeds'
-      }]
-    };
+    this.listItems = [{
+      id: 1,
+      title: 'Sales Cloud',
+      initials: 'SC',
+      description: 'The primary internal Salesforce org. Used to run our...'
+    }, {
+      id: 2,
+      title: 'Marketing Cloud',
+      initials: 'MC',
+      description: 'Salesforce Marketing Cloud lets businesses of any size...'
+    }, {
+      id: 3,
+      title: 'HR Concierge',
+      initials: 'HR',
+      description: 'Community for managing employee benefits and time off.'
+    }, {
+      id: 4,
+      title: 'My Money',
+      initials: 'MM',
+      description: 'Manage your finances across multiple financial platforms...'
+    }, {
+      id: 5,
+      title: 'Call Center',
+      initials: 'CC',
+      description: 'The key to call center and contact center management is more...'
+    }, {
+      id: 6,
+      title: 'Customer Support Community',
+      initials: 'CS',
+      description: 'Areas of Focus are used to track customer support for your...'
+    }, {
+      id: 7,
+      title: 'Salesforce Chatter',
+      initials: 'CH',
+      description: 'The Salesforce Chatter social network, including profiles and feeds'
+    }];
   }
 
   render () {
     return (
       <div>
-        <SortableList listItems={this.state.listItems} />
+        <SortableList listItems={this.listItems} />
       </div>
     )
   }
